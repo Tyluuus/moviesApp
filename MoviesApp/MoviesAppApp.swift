a37f930da50b4043586d56fc4804a6c1c5705d8a
Piotr Tyl <tyl.piotrr@gmail.com>
2023-06-10 19:26:11 +0200

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Piotr Tyl on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
